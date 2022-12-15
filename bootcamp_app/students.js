83133d570dc5c0423932b184b07373ee4e459fea
ruilinqi <ruilinqi7@gmail.com>
2022-12-15 05:27:56 +0000

    Update students.js file to use parameterized queries

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,14 +7,21 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+// Parameterized Query
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`, limit];
+console.log(values);
+
 // Querying The Database
 pool.query(`
 SELECT students.id as id, students.name as name, cohorts.name as cohort_name
 FROM students
 JOIN cohorts ON cohorts.id = students.cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`)
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort_name} cohort`);
