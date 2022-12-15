78df6732bfe9381a4a1c08def2ccdeb0698a8d83
ruilinqi <ruilinqi7@gmail.com>
2022-12-15 02:45:30 +0000

    Connected to a PostgreSQL database from a JavaScript app using the node-postgres library

---
+++ bootcamp_app/students.js
@@ -0,0 +1,24 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+// Querying The Database
+pool.query(`
+SELECT students.id as id, students.name as name, cohorts.name as cohort_name
+FROM students
+JOIN cohorts ON cohorts.id = students.cohort_id
+WHERE cohorts.name LIKE '%${process.argv[2]}%'
+LIMIT ${process.argv[3] || 5};
+`)
+.then(res => {
+  res.rows.forEach(user => {
+    console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort_name} cohort`);
+  })
+}).catch(err => console.error('query error', err.stack));
+
+
